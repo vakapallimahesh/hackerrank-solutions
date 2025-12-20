0c439d0f79f3a51132099dfd23a3032c6cd292c7
vakapallimahesh <vakapallimahesh48@gmail.com>
2025-12-20 21:59:10 +0530

    Add files via upload

---
+++ 22.py
@@ -0,0 +1,15 @@
+def print_formatted(number):
+    width = len(bin(number)[2:])
+
+    for i in range(1, number + 1):
+        dec = str(i).rjust(width)
+        octal = oct(i)[2:].rjust(width)
+        hexa = hex(i)[2:].upper().rjust(width)
+        binary = bin(i)[2:].rjust(width)
+
+        print(dec, octal, hexa, binary)
+    # your code goes here
+ 
+if __name__ == '__main__':
+    n = int(input())
+    print_formatted(n)
